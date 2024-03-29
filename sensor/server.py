ea2f0fff7e4cb8156a9797f03d6d357da5bc87c2
egoebelbecker <eric@ericgoebelbecker.com>
2024-03-29 12:37:14 -0400

    Server changes

--- sensor/server.py
+++ sensor/server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 number = "7"
 
 @app.route('/')
 def index():
     return render_template('index.html', number=number)
+
+@app.route('/readings.txt')
+def get_data():
+    try:
+	    return send_file('readings.txt', attachment_filename='readings.txt')
+    except Exception as e:
+		return str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
