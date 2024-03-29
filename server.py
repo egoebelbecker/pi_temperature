b527f6d750d0c8cbdd94e1b11fdb653b464be44d
Eric G <eric@ericgoebelbecker.com>
2024-03-29 14:13:31 -0400

    Done? Ish?

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
-number = "7"
 
 @app.route('/')
 def index():
-    return render_template('fetch.html', number=number)
+    return render_template('fetch.html')
 
 @app.route('/readings.txt')
 def get_data():
     try:
-       return send_file('/home/pi/pi_temperature/sensor/readings.txt', mimetype="text/csv", cache_timeout=0)
+       return send_file('/home/pi/pi_temperature/readings.txt', mimetype="text/csv", cache_timeout=0)
     except Exception as e:
        return str(e)
 
